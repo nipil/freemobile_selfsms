ad13d98319f7b23074672420bd8b4756eef5eb70
nipil <nipil@users.noreply.github.com>
2016-10-22 10:23:47 +0200

    add debugging (use --log DEBUG)

--- sms.py
+++ sms.py
@@ -3,13 +3,42 @@
 
 from __future__ import print_function
 
-import os, sys, requests, ConfigParser
+import os, sys, requests, ConfigParser, argparse, logging
 
 def eprint(*args, **kwargs):
 
     print(*args, file=sys.stderr, **kwargs)
 
+def activate_debug():
+
+    # These two lines enable debugging at httplib level (requests->urllib3->http.client)
+    # You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+    # The only thing missing will be the response.body which is not logged.
+    try:
+        import http.client as http_client
+    except ImportError:
+        # Python 2
+        import httplib as http_client
+    http_client.HTTPConnection.debuglevel = 1
+
+    # You must initialize logging, otherwise you'll not see debug output.
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log', help='choix du niveau de log', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL', 'EXCEPTION'], default='WARNING')
+    return parser.parse_args()
+
 def main():
+
+    args = parse_arguments()
+
+    if args.log == 'DEBUG':
+        activate_debug()
 
     config = ConfigParser.ConfigParser()
     config.read(os.path.expanduser('~/.sms.py.conf'))
